f06a1bc8a87a40a1e7d7ad31e6e8549f1dfb53df
Eric Chamberlain <eric.chamberlain@hotmail.com>
2018-06-14 09:34:32 -0700

    Initial Commit

---
+++ Analytics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Analytics
+//
+//  Created by Eric Chamberlain on 6/14/18.
+//  Copyright © 2018 Eric Chamberlain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
