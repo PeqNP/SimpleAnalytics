15e59140b869b065a5d00e7a30e52eaf1f5f3860
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-03-15 07:55:04 -0700

    Updated tests. Did not include tests for transactions.

--- AnalyticsTests/Fake/FakeAnalyticsListener.swift
+++ AnalyticsTests/Fake/FakeAnalyticsListener.swift
@@ -11,9 +11,14 @@ class FakeAnalyticsListener: AnalyticsListener, Spryable {
     
     enum Function: String, StringRepresentable {
         case receive = "receive"
+        case transaction = "transaction"
     }
     
     func receive(_ event: AnalyticsEvent) {
         return spryify(arguments: event)
+    }
+    
+    func transaction(_ event: AnalyticsEvent, _ context: AnalyticsTransaction) {
+        return spryify(arguments: event, context)
     }
 }
